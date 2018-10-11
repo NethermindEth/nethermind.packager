9400a0df9b646aa56ae3a93d694550fa8d3de258
Piotr Gankiewicz <spetz90@gmail.com>
2018-10-11 21:00:53 +0200

    Fixed file upload size

--- src/Nethermind.Packager.Web/Controllers/FilesController.cs
+++ src/Nethermind.Packager.Web/Controllers/FilesController.cs
@@ -17,6 +17,7 @@ namespace Nethermind.Packager.Web.Controllers
         }
        
         [HttpPost]
+        [RequestSizeLimit(200_000_000)]
         public async Task<ActionResult> Post(List<IFormFile> files, string apiKey)
         {
             await _packageUploader.UploadAsync(files, apiKey);
